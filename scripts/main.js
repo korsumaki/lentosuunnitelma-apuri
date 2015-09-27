6c2124e97e8863222fe02f9f7450213519786110
Markku Korsumäki <markku.korsumaki@gmail.com>
2015-09-27 14:27:19 +0300

    Changes to airspace 12 NOV 2015

--- scripts/main.js
+++ scripts/main.js
@@ -145,17 +145,17 @@ var gSelectedFlyingTimeStr;
 // constants
 
 // Date when update is taken into use.
-var dateOfUpdateStr = "2015-07-23T00:00:00Z";
+var dateOfUpdateStr = "2015-11-12T00:00:00Z";
 
 // These names are used before update
 var VFRPortFilename_before_change    = "EF_VFRREP_13NOV2014.xml";
-var AerodromesFilename_before_change = "aerodromes_28MAY2015.xml";
+var AerodromesFilename_before_change = "aerodromes_23JUL2015.xml";
 var ZZZZFieldsFilename_before_change = "zzzz_fields_28MAY2015.xml";
 
 // Current (new) filenames
-var VFRPortFilename = "EF_VFRREP_13NOV2014.xml"; // This does not change
-var AerodromesFilename = "aerodromes_23JUL2015.xml";
-var ZZZZFieldsFilename = "zzzz_fields_28MAY2015.xml";
+var VFRPortFilename = "EF_VFRREP_12NOV2015.xml";
+var AerodromesFilename = "aerodromes_12NOV2015.xml";
+var ZZZZFieldsFilename = "zzzz_fields_28MAY2015.xml"; // does not change in this update
 
 var UNOFFICIAL_AERODROME="ZZZZ";
 var UNOFFICIAL_AERODROME_INDEX=0; // This field index is used when ZZZZ place name was written manually, not from xml.
